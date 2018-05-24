5f428c2e3a26e91e60e5122a958b52614a66523c
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-24 22:06:26 +0100

    maps editor class

--- src/main/java/models/Editor.java
+++ src/main/java/models/Editor.java
@@ -1,9 +1,30 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="editors")
+
 public class Editor extends Journalist {
     private int id;
 
     public Editor() {
     }
 
+    public Editor(String username) {
+        super(username);
+    }
+
+    //TO DO WRITE EDITOR AUTHORISATION OF ENUM IN THIS CLASS
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
