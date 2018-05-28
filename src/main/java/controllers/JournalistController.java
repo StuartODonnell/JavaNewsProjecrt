80389a819db03befd08435586bfc7cf373cd690d
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-28 18:39:50 +0100

    edit journalist functionality working

--- src/main/java/controllers/JournalistController.java
+++ src/main/java/controllers/JournalistController.java
@@ -21,19 +21,17 @@ public class JournalistController {
 
     private void setupEndpoints() {
 
-//        get("/journalists/:id/edit", (req, res) -> {
-//            String stringId = req.params(":id");
-//            Integer integerId = Integer.parseInt(stringId);
-//            Journalist journalist = DBHelper.find(Journalist.class, integerId);
-//
-//            Map<String, Object> model = new HashMap<>();
-////            String loggedInUser = LoginController.getLoggedInUserName(req, res);
-////            model.put("user", loggedInUser);
-//            model.put("journalist", journalist);
-//            model.put("template", "templates/journalists/edit.vtl");
-//
-//            return new ModelAndView(model, "templates/layout.vtl");
-//        }, new VelocityTemplateEngine());
+        get("/journalists/:id/edit", (req, res) -> {
+            Integer integerId = Integer.parseInt(req.params("id"));
+            Journalist journalist = DBHelper.find(Journalist.class, integerId);
+            Map<String, Object> model = new HashMap<>();
+//            String loggedInUser = LoginController.getLoggedInUserName(req, res);
+//            model.put("user", loggedInUser);
+            model.put("journalist", journalist);
+            model.put("template", "templates/journalists/edit.vtl");
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
 
         get("/journalists", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
@@ -90,18 +88,17 @@ public class JournalistController {
 //            res.redirect("/journalists");
 //            return null;
 //        }, new VelocityTemplateEngine());
-//
-//        post ("/journalists/:id", (req, res) -> {
-//            String stringId = req.params(":id");
-//            Integer integerId = Integer.parseInt(stringId);
-//            Journalist journalist = DBHelper.find(Journalist.class, integerId);
-//            String name = req.queryParams("name");
-//            journalist.setName(name);
-//            DBHelper.save(journalist);
-//            res.redirect("/journalists");
-//            return null;
-//
-//        }, new VelocityTemplateEngine());
+
+        post ("/journalists/:id", (req, res) -> {
+            String stringId = req.params(":id");
+            Integer integerId = Integer.parseInt(stringId);
+            Journalist journalist = DBHelper.find(Journalist.class, integerId);
+            String name = req.queryParams("name");
+            journalist.setName(name);
+            DBHelper.save(journalist);
+            res.redirect("/journalists");
+            return null;
+        }, new VelocityTemplateEngine());
 
     }
 }
